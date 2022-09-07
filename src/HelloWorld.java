d022b01ceb3d62215202faf5c85b2ed5ce946c9c
mohammedkeyseaddib <Mohammedkadd@hotmail.com>
2022-09-07 09:44:47 +0200

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+
+        System.out.println("Teller til 10");
+        for (int i=0; i<=10; ++i){
+            System.out.println(i);
+        }
+    }
+}
